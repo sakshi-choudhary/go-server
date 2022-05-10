b3813305b83114f19111297eef9173906cf01f35
Sakshi Choudhary <choudharysakshi023@gmail.com>
2022-05-10 23:10:00 +0530

    🐛 charset fix

--- main.go
+++ main.go
@@ -7,12 +7,11 @@ import (
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "POST request sucessfull")
+	fmt.Fprintf(w, "POST request successful\n")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
 	fmt.Fprintf(w, "Name = %s\n", name)
@@ -24,20 +23,20 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
-
 	if r.Method != "GET" {
 		http.Error(w, "method is not supported", http.StatusNotFound)
 		return
 	}
-	fmt.Fprint(w, "hello!")
+	fmt.Fprintf(w, "hello!")
 }
+
 func main() {
-	fileServer := http.FileServer((http.Dir("./static")))
+	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Printf(("Starting server at port 8080\n"))
+	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
