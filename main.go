111d83bfd71291d8ecf0c362386bac1239f01d3e
Sakshi Choudhary <choudharysakshi023@gmail.com>
2022-05-10 21:56:23 +0530

    ✏️ typos

--- main.go
+++ main.go
@@ -6,16 +6,17 @@ import (
 	"net/http"
 )
 
-func formHandler(w http.ResponseWriter, r *http.Request){err := r.ParseForm()
-	if err := r.ParseForm(); err!=nil{
-		fmt.Fprintf(w."ParseForm() err: %v",err)
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w,"POST request sucessfull")
+	fmt.Fprintf(w, "POST request sucessfull")
 	name := r.FormValue("name")
-	address := r.Formvalue("address")
-	fmt.Fprintf(w, "Name = %s\n",name)
-	fmt.Fprintf(w, "Address = %s\n",address)
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
